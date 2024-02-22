d9f9b05d19f52b14fc4eae9a6faf31ec1f2291f4
Armaan <armaandeepnagra@icloud.com>
2024-02-22 11:10:30 +0000

    Made some of the settings neater

--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@ import pygame
 pygame.mixer.init()
 pygame.font.init()
 
-#load background and music
+#welcome and name input screen
 main_background = pygame.image.load('Stages/Stage 1/monkey.png')
 message1 = pygame.image.load('Stages/Stage 2/message1.png')
 message2 = pygame.image.load('Stages/Stage 2/message2.png')
 background_music = pygame.mixer.Sound("Stages/Stage 1/background.mp3")
+inside_input_box = False
+input_rect = pygame.Rect(500,700,140,100)
+name_placeholder = ''
 
 #colours
 white = (255,255,255)
@@ -24,22 +27,6 @@ name_score_font = pygame.font.Font(None,50)
 leaderboard_font = pygame.font.Font(None,100)
 level_font = pygame.font.Font(None,50)
 
-#name
-name_placeholder = ''
-
-#creating rectangle
-input_rect = pygame.Rect(500,700,140,100)
-
-#check to see if user is inside_box
-inside_input_box = False
-
-#gunshot
-gunshot_sound = pygame.mixer.Sound("Stages/Stage 5/sounds/uzi-new.mp3")
-
-#bullets
-bullets = 100
-
-
 #uzi
 uzi = pygame.image.load('Stages/Stage 5/photos/uzi-new.png')
 gunshot_sound = pygame.mixer.Sound("Stages/Stage 5/sounds/uzi-new.mp3")
@@ -51,7 +38,7 @@ grenade_sound = pygame.mixer.Sound("Stages/Stage 5/sounds/explosion.mp3")
 grenade_visual = pygame.image.load('Stages/Stage 5/photos/INGAME/explosion.png')
 
 #level background
-background_image = pygame.image.load('Stages/Stage 5/photos/backgroundtest.png')
+background_image = pygame.image.load('Stages/Stage 5/photos/level_background.png')
 
 #scrolling speed of background
 scroll_speed = 0.5
